8661f05fbd028a2acf9f3f2db84607501a638634
Mineth Weerasinghe <minethweerasinghe@gmail.com>
2025-01-17 11:10:26 +0530

    started integrating asgardeo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import { AuthProvider } from "@asgardeo/auth-react";
+
 
 const App: React.FC = () => {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from "@asgardeo/auth-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const config = {
+  signInRedirectURL: "https://localhost:3000/sign-in",
+  signOutRedirectURL: "https://localhost:3000/sign-out",
+  clientID: "{8RBXh7Ipwl00F_jKW68UaTAImNca}",
+  baseUrl: "https://api.asgardeo.io/t/min3thindustries",
+  scope: [ "openid profile" ]
+};
+
+const MyApp = () => {
+  return (
+    <AuthProvider config={config}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(<MyApp />);
